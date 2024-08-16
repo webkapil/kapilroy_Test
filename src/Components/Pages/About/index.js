f02ea017deb923a8cb95f16750c629abeaa0d945
webkapil <kapilroy6000@gmail.com>
2024-08-16 22:12:36 +0530

    first upload

---
+++ src/Components/Pages/About/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div style={{
+            height: "200px",
+            padding: "300px 0 0 0",
+        }}>
+            About
+        </div>
+    )
+}
+
+export default About
